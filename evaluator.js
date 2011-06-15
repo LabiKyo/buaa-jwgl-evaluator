d979b8cdcad9a24db6424ae49ce8537295e5a084
Labi Kyo <labikyo@gmail.com>
2011-06-16 03:44:39 +0800

    js codes done;

--- evaluator.js
+++ evaluator.js
@@ -1,1 +1,51 @@
-javascript:void();
+flag = true;
+id = '';
+function evaluator() {
+  id = setInterval('evaluate()', 50); // 0.05s刷一次
+  setInterval('checkFlag()', 50);
+}
+function checkFlag() {
+  if (!flag) {
+    clearInterval(id);
+  }
+}
+function evaluate() {
+  // 评价一位教师
+  try { // POST的时候运行函数会导致异常
+    var i;
+    iframe = document.getElementById('iframeautoheight');
+    iDoc = iframe.contentWindow || iframe.contentDocument; // iDoc是iframe中的document
+    if (iDoc.document) {
+      iDoc = iDoc.document;
+    }
+
+    trs = iDoc.getElementById('divJs').getElementsByTagName('tr'); // 获取所有tr
+    j = 1;
+    selects = trs[j].getElementsByTagName('select'); // 获取第一行的所有select,因为有些课程不止一个教师
+    if (selects.length) {
+      fillTr(selects, 2);
+    } else {
+      ++j;
+      selects = trs[j].getElementsByTagName('select'); // 有照片的时候情况很奇葩..
+      fillTr(selects, 2);
+    }
+    for (i = j + 1; i < trs.length; ++i) { // 遍历行,评价A
+      selects = trs[i].getElementsByTagName('select'); // 获取该行select
+      fillTr(selects, 1);
+    }
+    var submit = iDoc.getElementById('Button1'); // 获取按钮
+    submit.click() // 提交请求
+      course = iDoc.getElementById('pjkc'); // 获取评价课程select
+    if (course.selectedIndex === course.length - 1) { // 这是最后一个课程了!
+      flag = false;
+    }
+  } catch (e) { // 做静默处理
+  }
+}
+
+function fillTr(elems, target) { // 给某一行的所有select赋值
+  var i;
+  for (i = 0; i < elems.length; ++i) {
+    elems[i].selectedIndex = target;
+  }
+}
