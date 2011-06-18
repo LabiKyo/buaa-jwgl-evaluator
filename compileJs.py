2591295b038a83216ab58ace4073eceb00a98bf0
Labi Kyo <labikyo@gmail.com>
2011-06-19 02:14:04 +0800

    add python script to concatenate all lines to one line; one line js works in chrome and ff;

---
+++ compileJs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# compilor for evaluator.js
+# @author: Labi Kyo <labikyo@gmail.com>
+
+import re
+
+def main():
+    try:
+        fin = open('./evaluator.js', 'r')
+        fout = open('./script.js', 'w')
+        fout.write('javascript:')
+        remove_comment = re.compile(r'(.+)//.+')
+        for line in fin:
+            result = remove_comment.match(line)
+            if result:
+                line = result.group(1) # match comment, remove it
+            line = re.sub('^\s+', '', line) # remove start spaces
+            line = re.sub('\s+$', '', line) # remove end spaces
+            fout.write(line.translate(None, '\n').translate(None, '\r'))
+            # remove end of line
+        fin.close()
+        fout.close()
+    except IOError:
+        pass
+
+if __name__ == '__main__':
+    main()
--- evaluator.js
+++ evaluator.js
@@ -1,27 +1,24 @@
 flag = true;
 id = '';
-function evaluator() {
-  id = setInterval('evaluate()', 50); // 0.05s刷一次
-  setInterval('checkFlag()', 50);
-}
-function checkFlag() {
-  if (!flag) {
-    clearInterval(id);
+function fillTr(elems, target) { // 给某一行的所有select赋值
+  var i;
+  for (i = 0; i < elems.length; ++i) {
+    elems[i].selectedIndex = target;
   }
 }
 function evaluate() {
   // 评价一位教师
   try { // POST的时候运行函数会导致异常
     var i;
-    iframe = document.getElementById('iframeautoheight');
-    iDoc = iframe.contentWindow || iframe.contentDocument; // iDoc是iframe中的document
+    var iframe = document.getElementById('iframeautoheight');
+    var iDoc = iframe.contentWindow || iframe.contentDocument; // iDoc是iframe中的document
     if (iDoc.document) {
       iDoc = iDoc.document;
     }
 
-    trs = iDoc.getElementById('divJs').getElementsByTagName('tr'); // 获取所有tr
-    j = 1;
-    selects = trs[j].getElementsByTagName('select'); // 获取第一行的所有select,因为有些课程不止一个教师
+    var trs = iDoc.getElementById('divJs').getElementsByTagName('tr'); // 获取所有tr
+    var j = 1;
+    var selects = trs[j].getElementsByTagName('select'); // 获取第一行的所有select,因为有些课程不止一个教师
     if (selects.length) {
       fillTr(selects, 2);
     } else {
@@ -34,20 +31,22 @@ function evaluate() {
       fillTr(selects, 1);
     }
     var submit = iDoc.getElementById('Button1'); // 获取按钮
-    submit.click() // 提交请求
-      course = iDoc.getElementById('pjkc'); // 获取评价课程select
+    submit.click(); // 提交请求
+    course = iDoc.getElementById('pjkc'); // 获取评价课程select
     if (course.selectedIndex === course.length - 1) { // 这是最后一个课程了!
       flag = false;
     }
   } catch (e) { // 做静默处理
   }
 }
-
-function fillTr(elems, target) { // 给某一行的所有select赋值
-  var i;
-  for (i = 0; i < elems.length; ++i) {
-    elems[i].selectedIndex = target;
+function checkFlag() {
+  if (!flag) {
+    clearInterval(id);
   }
+}
+function evaluator() {
+  id = setInterval('evaluate()', 50); // 0.05s刷一次
+  setInterval('checkFlag()', 50);
 }
 
 evaluator(); // 调用函数
